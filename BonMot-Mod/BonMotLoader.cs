fb6bb76bdb8d6a3951c6f1c8318785895b474c7d
Joe Zeiler <heavysleeper3@gmail.com>
2023-10-04 10:28:41 -0400

    updates to address issues found in the dawnsbury days discord.

--- BonMot-Mod/BonMotLoader.cs
+++ BonMot-Mod/BonMotLoader.cs
@@ -3,28 +3,35 @@ using Dawnsbury.Core.Mechanics.Enumerations;
 using Dawnsbury.Core.Mechanics;
 using Dawnsbury.Core.CombatActions;
 using Dawnsbury.Core.Mechanics.Targeting;
-using Dawnsbury.Core.Creatures;
 using Dawnsbury.Modding;
 using Dawnsbury.Core.Possibilities;
 using Dawnsbury.Core.Mechanics.Core;
-using System.Collections.Generic;
-
 namespace Dawnsbury.Mods.Feats.General.BonMot;
 
-public class ABetterFleetLoader
+/// <summary>
+/// loads the Bon Mot feat
+/// </summary>
+public class BonMotLoader
 {
+    /// <summary>
+    /// here to add the linguistic trait
+    /// </summary>
     public static Trait Linguistic;
     [DawnsburyDaysModMainMethod]
     public static void LoadMod()
     {
+        //registering the linguistic trait so we can add it to Bon Mot
         Linguistic = ModManager.RegisterTrait(
             "Linguistic",
-            new TraitProperties("Linguistic",true, "Only works on humanoid enemies")
+            new TraitProperties("Linguistic",true, "Only works on enemies that speak common")
             );
-        // This sample demonstrates how to replace an existing feat with a new one:
         ModManager.AddFeat(CreateBonMotFeat());
     }
 
+    /// <summary>
+    /// creates the Bon Mot feat, which adds the available Bot Mot action which can cause negatives to will saves and perception
+    /// </summary>
+    /// <returns>the created feat</returns>
     private static Feat CreateBonMotFeat()
     {
 
@@ -34,9 +41,9 @@ public class ABetterFleetLoader
                 "You launch an insightful quip at a foe, distracting them.",
                 "Choose a foe within 30 feet and roll a Diplomacy check against the target's Will DC.\n\nCritical Success: The target is distracted and takes a –3 status penalty to Perception and Will saves for 1 minute. " +
                 "The target can end the effect early with a retort to your Bon Mot. This can either be a single action that has the concentrate trait or an appropriate skill action to frame their retort. " +
-                "The GM determines which skill actions qualify, though they must take at least 1 action. Typically, the retort needs to use a linguistic Charisma-based skill action.\r\nSuccess: As critical success, but the penalty is –2." +
+                "\r\nSuccess: As critical success, but the penalty is –2." +
                 "\r\nCritical Failure: Your quip is atrocious. You take the same penalty an enemy would take had you succeeded. This ends after 1 minute or if you issue another Bon Mot and succeed.",
-                new[] { Trait.Auditory, Trait.Concentrate, Trait.Emotion, Trait.General, Trait.Mental, Trait.Skill })
+                new[] { Trait.Auditory, Trait.Concentrate, Trait.Emotion, Trait.General, Trait.Mental, Trait.Skill, Linguistic })
             .WithActionCost(1)
             .WithCustomName("Bon Mot")
             .WithPrerequisite(C =>
@@ -48,25 +55,22 @@ public class ABetterFleetLoader
                     ProvideMainAction = (qfself)=>
                     {
                         var dude = qfself.Owner;
-                        CombatAction bonmotAction = new CombatAction(dude, null, "Bon Mot", new Trait[] { Trait.Auditory, Trait.Concentrate, Trait.Emotion, Trait.General, Trait.Mental, Trait.Skill },
+                        CombatAction bonmotAction = new CombatAction(dude, null, "Bon Mot", new Trait[] { Trait.Auditory, Trait.Concentrate, Trait.Emotion, Trait.General, Trait.Mental, Trait.Skill, Linguistic },
                             "Choose a foe within 30 feet and roll a Diplomacy check against the target's Will DC.\n\nCritical Success: The target is distracted and takes a –3 status penalty to Perception and Will saves for 1 minute. " +
                             "The target can end the effect early with a retort to your Bon Mot. This can either be a single action that has the concentrate trait or an appropriate skill action to frame their retort. " +
-                            "The GM determines which skill actions qualify, though they must take at least 1 action. Typically, the retort needs to use a linguistic Charisma-based skill action.\r\nSuccess: As critical success, but the penalty is –2." +
+                            "\r\nSuccess: As critical success, but the penalty is –2." +
                             "\r\nCritical Failure: Your quip is atrocious. You take the same penalty an enemy would take had you succeeded. This ends after 1 minute or if you issue another Bon Mot and succeed.",
-                            Target.RangedCreature(6));//.WithAdditionalConditionOnTargetCreature((caster, target) => target.HasTrait(Trait.Humanoid) || target.HasTrait(Trait.Orc) ? Usability.Usable : Usability.NotUsableOnThisCreature("Target cannot understand your witty remarks")));
+                            Target.RangedCreature(6).WithAdditionalConditionOnTargetCreature((caster, target) => target.DoesNotSpeakCommon ? Usability.NotUsableOnThisCreature("Target cannot understand your witty remarks") : Usability.Usable));
 
                         return new ActionPossibility(bonmotAction
                             .WithActionCost(1)
-                            .WithActiveRollSpecification(new ActiveRollSpecification(
-                                (CombatAction action, Creature attacker, Creature target) =>
-                                    { return new CalculatedNumber(attacker.Proficiencies.Get(Trait.Diplomacy).ToNumber(attacker.Level) + attacker.Abilities.Charisma, "Diplomacy", new List<Bonus>()); }
-                                , (CombatAction action, Creature attacker, Creature target) =>
-                                    { return new CalculatedNumber(target.Defenses.GetBaseValue(Defense.Will) + 10, "target will DC", target.Defenses.DetermineDefenseBonuses(attacker, bonmotAction, Defense.Will, target)); }))
+                            .WithActiveRollSpecification(
+                            new ActiveRollSpecification(Checks.SkillCheck(Skill.Diplomacy), Checks.DefenseDC(Defense.Will)))
                             .WithEffectOnEachTarget(async (spell, caster, target, result) =>
                             {
                                 if (result == CheckResult.CriticalSuccess)
                                 {
-                                    target.AddQEffect(new QEffect("Bon Mot", "-3 status penalty to Perception and Will saves", ExpirationCondition.CountsDownAtEndOfYourTurn, caster)
+                                    target.AddQEffect(new QEffect("Bon Mot Crit Sucess", "-3 status penalty to Perception and Will saves", ExpirationCondition.CountsDownAtEndOfYourTurn, caster)
                                     {
                                         BonusToDefenses = (qfSelf, incomingEffect, targetedDefense) =>
                                         {
@@ -75,13 +79,21 @@ public class ABetterFleetLoader
                                                 return new Bonus(-3, BonusType.Status, "Bon Mot Critical Success", false);
                                             }
                                             return null;
+                                        },
+                                        BonusToAttackRolls = (qfself, incomingEffect, targetedCreature) =>
+                                        {
+                                            if (incomingEffect.ActionId == ActionId.Seek)
+                                            {
+                                                return new Bonus(-3, BonusType.Status, "Bon Mot Critical Success", false);
+                                            }
+                                            return null;
                                         }
-                                    });
+                                    }.WithExpirationAtStartOfSourcesTurn(caster, 10));
                                     caster.RemoveAllQEffects(q => q.Name == "Bon Mot Critical Failure");
                                 }
                                 else if (result == CheckResult.Success)
                                 {
-                                    target.AddQEffect(new QEffect("Bon Mot", "-2 status penalty to Perception and Will saves", ExpirationCondition.CountsDownAtEndOfYourTurn, caster)
+                                    target.AddQEffect(new QEffect("Bon Mot Success", "-2 status penalty to Perception and Will saves", ExpirationCondition.CountsDownAtEndOfYourTurn, caster)
                                     {
                                         BonusToDefenses = (qfSelf, incomingEffect, targetedDefense) =>
                                         {
@@ -90,8 +102,16 @@ public class ABetterFleetLoader
                                                 return new Bonus(-2, BonusType.Status, "Bon Mot Success", false);
                                             }
                                             return null;
+                                        },
+                                        BonusToAttackRolls = (qfself, incomingEffect, targetedCreature) =>
+                                        {
+                                            if (incomingEffect.ActionId == ActionId.Seek)
+                                            {
+                                                return new Bonus(-2, BonusType.Status, "Bon Mot Success", false);
+                                            }
+                                            return null;
                                         }
-                                    });
+                                    }.WithExpirationAtStartOfSourcesTurn(caster, 10));
                                     caster.RemoveAllQEffects(q => q.Name == "Bon Mot Critical Failure");
                                 }
                                 if (result == CheckResult.CriticalFailure)
@@ -105,8 +125,16 @@ public class ABetterFleetLoader
                                                 return new Bonus(-2, BonusType.Status, "Bon Mot Critical Failure", false);
                                             }
                                             return null;
+                                        },
+                                        BonusToAttackRolls = (qfself, incomingEffect, targetedCreature) =>
+                                        {
+                                            if (incomingEffect.ActionId == ActionId.Seek)
+                                            {
+                                                return new Bonus(-2, BonusType.Status, "Bon Mot Critical Failure", false);
+                                            }
+                                            return null;
                                         }
-                                    });
+                                    }.WithExpirationAtStartOfSourcesTurn(caster, 10));
                                 }
 
                             }));
