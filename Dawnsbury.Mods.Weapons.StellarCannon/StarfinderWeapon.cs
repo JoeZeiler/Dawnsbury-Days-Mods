080e9ba666362010cfa3b4fe7a46a60ee44d88ed
Joe Zeiler <heavysleeper3@gmail.com>
2023-11-08 22:23:48 -0500

    adding auto firing mechanic (using field test rules, not the newer rules shown in playtest stream because those would be considerably more complicated). added Rotolaser as an auto firing gun and laser pistol as just a normal gun. Reorganized a lot.

---
+++ Dawnsbury.Mods.Weapons.StellarCannon/StarfinderWeapon.cs
@@ -0,0 +1,123 @@
+﻿using Dawnsbury.Core.Mechanics.Enumerations;
+using Dawnsbury.Core.Mechanics.Treasure;
+using Dawnsbury.Core;
+
+namespace Dawnsbury.Mods.Weapons.StarfinderWeapons
+{
+    /// <summary>
+    /// stores ephemeral properties for area weapons
+    /// </summary>
+    public class EphemeralAreaProperties : EphemeralItemProperties
+    {
+        public int CurrentMagazine
+        {
+            get; set;
+        }
+    }
+
+    /// <summary>
+    /// stores properties for guns
+    /// </summary>
+    public class GunItem : Item
+    {
+        public GunItem(IllustrationName illustration, string name, params Trait[] traits) : base(illustration, name, traits)
+        {
+
+        }
+        public GunItem(ItemName itemName, IllustrationName illustration, string name, int level, int price, params Trait[] traits) : base(itemName, illustration, name, level, price, traits)
+        {
+        }
+
+        public string BaseGunItemName
+        {
+            get; set;
+        }
+
+        public int Usage
+        {
+            get; set;
+        } = 1;
+
+        public int CommercialUsage
+        {
+            get; set;
+        } = 1;
+        public int TacticalUsage
+        {
+            get; set;
+        } = 1;
+        public int AdvancedUsage
+        {
+            get; set;
+        } = 2;
+
+        public int Capacity
+        {
+            get; set;
+        } = 5;
+
+        public int CommercialCapacity
+        {
+            get; set;
+        } = 5;
+        public int TacticalCapacity
+        {
+            get; set;
+        } = 10;
+        public int AdvancedCapacity
+        {
+            get; set;
+        } = 10;
+
+        public int CommercialRange
+        {
+            get; set;
+        } = 6;
+        public int TacticalRange
+        {
+            get; set;
+        } = 8;
+        public int AdvancedRange
+        {
+            get; set;
+        } = 8;
+
+        public int Reload
+        {
+            get; set;
+        } = 1;
+    }
+
+    /// <summary>
+    /// stores properties for area items
+    /// </summary>
+    public class AreaItem : GunItem
+    {
+        public enum AreaTypes
+        {
+            Burst,
+            Cone,
+            Line
+        }
+        public AreaItem(IllustrationName illustration, string name, params Trait[] traits) : base(illustration, name, traits)
+        {
+
+        }
+
+        public AreaItem(ItemName itemName, IllustrationName illustration, string name, int level, int price, params Trait[] traits) : base(itemName, illustration, name, level, price, traits)
+        {
+        }
+        public AreaTypes AreaType
+        {
+            get; set;
+        } = AreaTypes.Burst;
+        public int AreaRange
+        {
+            get; set;
+        } = 1;
+        public int Tracking
+        {
+            get; set;
+        } = 0;
+    }
+}
