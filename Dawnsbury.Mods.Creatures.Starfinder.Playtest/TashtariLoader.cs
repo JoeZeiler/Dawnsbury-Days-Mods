108f9a1e2af0339758474539d342d3dba7f66b0d
Joe Zeiler <heavysleeper3@gmail.com>
2024-02-15 13:59:24 -0500

    technically working creature, hooray!

--- Dawnsbury.Mods.Creatures.Starfinder.Playtest/TashtariLoader.cs
+++ Dawnsbury.Mods.Creatures.Starfinder.Playtest/TashtariLoader.cs
@@ -1,99 +1,193 @@
 ﻿using Dawnsbury.Core;
+using Dawnsbury.Core.CombatActions;
 using Dawnsbury.Core.Creatures;
 using Dawnsbury.Core.Mechanics;
+using Dawnsbury.Core.Mechanics.Core;
 using Dawnsbury.Core.Mechanics.Enumerations;
+using Dawnsbury.Core.Tiles;
+using Dawnsbury.Core.Intelligence;
+using Dawnsbury.Core.Mechanics.Targeting;
 using Dawnsbury.Modding;
+using MapReaderUtility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Dawnsbury.Core.Mechanics.Targeting.Targets;
+using Dawnsbury.Core.Possibilities;
 
 namespace Dawnsbury.Mods.Creatures.Starfinder.Playtest
 {
     public class TashtariLoader
     {
+        public static Trait TashtariTechnical;
+        private static TiledMapData currentMap = null;
         /// <summary>
         /// loads the starfinder soldier mod. the Starfinder Weapons mod is a dependency
         /// </summary>
         [DawnsburyDaysModMainMethod]
         public static void LoadMod()
         {
-            ModManager.RegisterActionOnEachCreature(UpdateWolfToTashtari);
+            TashtariTechnical = ModManager.RegisterTrait("TashtariTechnical", new TraitProperties("TashtariTechnical", false));
+            ModManager.RegisterActionOnEachCreature(UpdateCreatureToTashtari);
         }
 
 
-        private static Action<Creature> UpdateWolfToTashtari = (Creature creature) =>
+        private static Action<Creature> UpdateCreatureToTashtari = (Creature creature) =>
         {
-            if (creature.MainName.Equals("Blood Wolf"))// && creature.Battle.Encounter.MapFilename.ToLower().Contains("starfinder"))
+            if(creature.Occupies == null || creature.HasTrait(Trait.Homebrew))
             {
-                creature.MainName = "Tashitari";
-                AddDistinguishesIfNeeded(creature);
-                creature.RemoveAllQEffects(q => q.Name.Contains("resistance") ||
-                                                q.Name.Contains("weakness") ||
-                                                q.Name.Contains("Attack of Opportunity") ||
-                                                q.Name.Contains("Pack Attack") ||
-                                                q.Name.Contains("Aggressive Rush"));
-                creature.Defenses.Set(Defense.AC, 19);
-                creature.Defenses.Set(Defense.Fortitude, 8);
-                creature.Defenses.Set(Defense.Reflex, 11);
-                creature.Defenses.Set(Defense.Will, 6);
-                creature.Abilities.Strength = 2;
-                creature.Abilities.Dexterity = 4;
-                creature.Abilities.Constitution = 0;
-                creature.Abilities.Intelligence = 1;
-                creature.Abilities.Wisdom = 0;
-                creature.Abilities.Charisma = -1;
-                creature.MaxHP = 42;
-                creature.WeaknessAndResistance.AddResistance(DamageKind.Fire, 5);
-                creature.BaseSpeed = 7;
-                creature.UnarmedStrike = new Core.Mechanics.Treasure.Item(IllustrationName.Jaws, "jaws", new[] {Trait.Melee, Trait.Unarmed, Trait.Weapon })
+                return;
+            }
+            if(currentMap == null || currentMap.MapName != creature.Battle.Encounter.MapFilename)
+            {
+                currentMap = new TiledMapData(creature.Battle.Encounter.MapFilename);
+            }
+            if (currentMap.GetValueAtLocationForLayer("Tashtari",creature.Occupies.X,creature.Occupies.Y) != 0)// && creature.Battle.Encounter.MapFilename.ToLower().Contains("starfinder"))
+            {
+                var oldOccupies = creature.Occupies;
+                var oldBattle = creature.Battle;
+                var oldFaction = creature.OwningFaction;
+                oldBattle.RemoveCreatureFromGame(creature);
+
+                var newCreature = new Creature(IllustrationName.BloodWolf256, "Tashtari", new[] { Trait.Beast, Trait.Homebrew, TashtariTechnical }, 3, 10, 7, 
+                                        new Core.Creatures.Parts.Defenses(19, 8, 11, 6), 42, 
+                                        new Core.Creatures.Parts.Abilities(2, 4, 0, 1, 0, -1), 
+                                        new Core.Creatures.Parts.Skills(acrobatics: 11, athletics: 9, stealth: 11));
+                newCreature.WeaknessAndResistance.AddResistance(DamageKind.Fire, 5);
+
+                var fireRayAttack = new Core.Mechanics.Treasure.Item(IllustrationName.FireRay, "muzzle beam", new[] { Trait.Ranged, Trait.Unarmed, Trait.Weapon, Trait.Fire })
+                {
+                    WeaponProperties = new Core.Mechanics.Treasure.WeaponProperties("1d8", DamageKind.Fire)
+                    {
+                        ItemBonus = 1,
+
+                    }.WithAdditionalDamage("4", DamageKind.Fire).WithAdditionalPersistentDamage("1d4", DamageKind.Fire).WithRangeIncrement(12),
+                };
+
+                newCreature.WithUnarmedStrike(fireRayAttack);
+
+                var jawsAttack = new Core.Mechanics.Treasure.Item(IllustrationName.Jaws, "jaws", new[] { Trait.Melee, Trait.Unarmed, Trait.Weapon })
                 {
                     WeaponProperties = new Core.Mechanics.Treasure.WeaponProperties("1d8", DamageKind.Piercing)
                     {
                         ItemBonus = 1,
                     }
                 };
-            }
-            if (creature.BaseName.Equals("Wolf"))
-            {
-                creature.MainName = "Tashitari Pup";
-                AddDistinguishesIfNeeded(creature);
-            }
-            creature.AddQEffect(new QEffect()
-            {
-                StateCheck = (qfSelf) =>
+
+                newCreature.WithAdditionalUnarmedStrike(jawsAttack);
+
+                int numberOfCreatures = 0;
+                bool flashUsed = false;
+
+                var BristleFlashTargeting = Target.SelfExcludingEmanation(8).WithIncludeOnlyIf((t, c) => !c.HasTrait(TashtariTechnical)) as EmanationTarget;
+                if (BristleFlashTargeting != null)
                 {
-                    foreach (Creature c in creature.Battle.AllCreatures.Where(cr => cr.BaseName.Equals("Blood Wolf") || cr.BaseName.Equals("Wolf")))
+                    BristleFlashTargeting.CreatureGoodness = (targeting, user, target) =>
                     {
-                        UpdateWolfToTashtari(c);
-                    }
+                        if(flashUsed)
+                        {
+                            return AIConstants.NEVER;
+                        }
+                        if (!target.FriendOf(user) && !target.QEffects.Any(q=>q.Id == QEffectId.Blinded && q.Name == QEffect.Dazzled().Name))
+                        {
+                            numberOfCreatures++;
+                        }
+                        if(target.FriendOf(user) && !target.HasTrait(TashtariTechnical))
+                        {
+                            numberOfCreatures--;
+                        }
+                        if(numberOfCreatures >= 2)
+                        {
+                            return AIConstants.EXTREMELY_PREFERRED;
+                        }
+                        return AIConstants.NEVER;
+                    };
                 }
-            });
+
+                CombatAction BristleFlash = new CombatAction(newCreature, IllustrationName.DazzlingFlash, "Bristle Flash", new[] { Trait.Visual, Trait.Light },
+                    "The tashtari causes its filaments to glow with intense light. " +
+                    "Non-tashtaris within a 40-foot emanation must attempt a DC 19 Fortitude save. A creature " +
+                    "that attempts this save is immune to all Bristle Flashes for 1 minute. The tashtari’s fur " +
+                    "loses its glow, and it can’t use this ability until it basks in sunlight for at least 10 minutes." +
+                    "\r\nCritical Success: The creature is unaffected." +
+                    "\r\nSuccess: The creature is dazzled for 1 round." +
+                    "\r\nFailure The creature is dazzled for 1 minute." +
+                    "\r\nCritical Failure The creature is blinded for 1 round and dazzled for 1 minute.", BristleFlashTargeting)
+                .WithActionCost(1).WithSavingThrow(new SavingThrow(Defense.Fortitude, (c) => 19))
+                .WithEffectOnEachTarget(async (action, user, target, result) =>
+                {
+                    if(result == CheckResult.CriticalSuccess)
+                    {
+                        return;
+                    }
+                    if(result == CheckResult.Success)
+                    {
+                        target.AddQEffect(QEffect.Dazzled().WithExpirationOneRoundOrRestOfTheEncounter(user, false));
+                    }
+                    if(result == CheckResult.Failure)
+                    {
+                        target.AddQEffect(QEffect.Dazzled().WithExpirationAtStartOfSourcesTurn(user, 10));
+                    }
+                    if( result == CheckResult.CriticalFailure) 
+                    {
+                        target.AddQEffect(QEffect.Blinded().WithExpirationOneRoundOrRestOfTheEncounter(user, false));
+                        target.AddQEffect(QEffect.Dazzled().WithExpirationAtStartOfSourcesTurn(user, 10));
+                    }
+                    numberOfCreatures = 0;
+                    flashUsed = true;
+                });
+
+                newCreature.AddQEffect(new QEffect("Bristle Flash", "Bright Filaments Glow to blind enemies")
+                {
+                    ProvideMainAction = (qfself)=>
+                    {
+                        return new ActionPossibility(BristleFlash);
+                    }
+                });
+
+                //newCreature.AddQEffect(new QEffect()
+                //{
+                //    StateCheck = (qfself) =>
+                //    {
+                //        foreach (Creature c in newCreature.Battle.AllCreatures)
+                //        {
+                //            if (newCreature.IsAdjacentTo(c) && newCreature.EnemyOf(c))
+                //            {
+                //                newCreature.ReplacementUnarmedStrike = jawsAttack;
+                //            }
+                //        }
+                //    }
+                //});
+
+                oldBattle.SpawnCreature(newCreature, oldFaction, oldOccupies);
+                
+            }
         };
 
-        private static void AddDistinguishesIfNeeded(Creature thisCreature)
-        {
-            List<Creature> list = thisCreature.Battle.AllCreatures.Where((Creature cr) => cr.MainName == thisCreature.MainName && cr != thisCreature).ToList();
-            if (list.Count == 0)
-            {
-                return;
-            }
+        //private static void AddDistinguishesIfNeeded(Creature thisCreature)
+        //{
+        //    List<Creature> list = thisCreature.Battle.AllCreatures.Where((Creature cr) => cr.MainName == thisCreature.MainName && cr != thisCreature).ToList();
+        //    if (list.Count == 0)
+        //    {
+        //        return;
+        //    }
 
-            char c = '\0';
-            foreach (Creature item in list)
-            {
-                if (item.Distinguisher == '\0')
-                {
-                    item.Distinguisher = 'A';
-                    c = 'A';
-                }
-                else if (item.Distinguisher > c)
-                {
-                    c = item.Distinguisher;
-                }
-            }
+        //    char c = '\0';
+        //    foreach (Creature item in list)
+        //    {
+        //        if (item.Distinguisher == '\0')
+        //        {
+        //            item.Distinguisher = 'A';
+        //            c = 'A';
+        //        }
+        //        else if (item.Distinguisher > c)
+        //        {
+        //            c = item.Distinguisher;
+        //        }
+        //    }
 
-            thisCreature.Distinguisher = (char)(c + 1);
-        }
+        //    thisCreature.Distinguisher = (char)(c + 1);
+        //}
 
 
     }
